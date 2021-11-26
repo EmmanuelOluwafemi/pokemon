02c0adc143bc3035cd132267810dedc6e4e66a55
EmmanuelOluwafemi <seek9ja@gmail.com>
2021-11-26 20:03:59 +0100

    added update

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const PokemonCard = ({ id, name, types, image, color }) => {
     return (
         <Link to={`/${name}`}>
-            <div className={`cursor-pointer py-4 px-8 rounded-lg ${color}`}>
+            <div className={`cursor-pointer hover:shadow-2xl py-4 px-8 rounded-lg ${color}`}>
                 <h1 className="text-2xl font-bold text-right opacity-20">{id}</h1>
                 <img className="w-24 h-24" src={image} alt={name} />
                 <h3 className="text-1xl font-bold text-center">{name}</h3>
